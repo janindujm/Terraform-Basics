4a5db2755ba10e4e679db43b026a9dc6ce50497a
janindujm <janindujm@gmail.com>
2025-04-20 20:46:20 +0530

    add output block

--- Excercise1/InstID.tf
+++ Excercise1/InstID.tf
@@ -1,8 +1,6 @@
 data "aws_ami" "amiId" {
   most_recent = true
 
-  
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
@@ -14,4 +12,11 @@ data "aws_ami" "amiId" {
   }
 
   owners = ["099720109477"] # Amazon's account ID for Ubuntu
+}
+
+
+
+output "instance_id" {
+    description = "AMI ID of ubuntu instance"
+    value       = data.aws_ami.amiId.id
 }
