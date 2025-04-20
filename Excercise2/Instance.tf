4012d26aff2d9c43230a60d7b51d5c29537cbbf6
janindujm <janindujm@gmail.com>
2025-04-20 22:32:57 +0530

    Add the instance

---
+++ Excercise2/Instance.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amiId.id
+  instance_type = "t3.micro"
+  key_name      = "dove-key"
+  vpc_security_group_ids = [aws_security_group.dove-sg.id]
+  availability_zone = "us-east-1a"
+
+
+  tags = {
+    Name = "Dove-Instance"
+    prject = "Dove"
+  }
+}
