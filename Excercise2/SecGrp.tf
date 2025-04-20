ce5d3e5ab390b2d04a15de08a19e47c8290e4187
janindujm <janindujm@gmail.com>
2025-04-20 23:16:37 +0530

    Create an instance

--- Excercise2/SecGrp.tf
+++ Excercise2/SecGrp.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "dove-sg" {
   name        = "dove-sg"
   description = "dove-sg"
-  vpc_id      = aws_vpc.main.id
+
 
   tags = {
     Name = "dove-sg"
@@ -10,15 +10,15 @@ resource "aws_security_group" "dove-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "sshfromyIP" {
   security_group_id = aws_security_group.dove-sg.id
-  cidr_ipv4         = ["175.157.170.134/32"]
-  from_port         = 22   
+  cidr_ipv4         = "175.157.170.134/32"
+  from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.dove-sg.id
-  cidr_ipv6         = ["0.0.0.0/0"]
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
